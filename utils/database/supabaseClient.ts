c76514c62c7f793e890ac3d3f8cb0487e44d07c7
DEPREMU <zae4758@gmail.com>
2025-08-07 20:58:58 -0600

    New begin

--- utils/database/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import Constants from "expo-constants";
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = Constants?.expoConfig?.extra?.SUPABASE_URL;
-const supabaseKey = Constants?.expoConfig?.extra?.SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
