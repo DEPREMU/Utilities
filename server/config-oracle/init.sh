eae2ee64f1afa4de28ba84ba6826396f7d78cb7a
DEPREMU <zae4758@gmail.com>
2025-08-24 00:53:49 -0600

    fix: update paths for start script in initialization script

--- server/config-oracle/init.sh
+++ server/config-oracle/init.sh
@@ -15,7 +15,7 @@ git pull origin mainVersion
 npm run install-all
 sudo ufw allow 3000
 
-chmod +x ./server/config-oracle/start.sh
-pm2 start ./start.sh --name Utilities
+chmod +x /home/ubuntu/Utilities/server/config-oracle/start.sh
+pm2 start /home/ubuntu/Utilities/server/config-oracle/start.sh --name Utilities
 pm2 save
 pm2 startup
