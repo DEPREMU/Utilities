1d5aed83c74a0b24d4820adbed1f29d9209650bb
DEPREMU <zae4758@gmail.com>
2026-05-05 12:19:57 -0600

    refactor: optimize storage service by removing unused functions and enhancing data parsing

--- app/src/utils/services/storage.ts
+++ app/src/utils/services/storage.ts
@@ -1,5 +1,8 @@
 import {
+  parseData,
   isSecureKey,
+  ServiceClass,
+  stringifyData,
   ALL_KEYS_STORAGE,
   SECURE_KEYS_STORAGE,
   ExpectedStorageTypes,
@@ -8,7 +11,6 @@ import {
   ALL_KEYS_STORAGE_KEYS,
   DO_NOT_DELETE_OR_SAVE,
   SECURE_KEYS_STORAGE_TYPE,
-  ServiceClass,
 } from "@common";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { cloneDeep } from "lodash";
@@ -63,131 +65,6 @@ type RemoveDataStorage = {
     key: T,
     callback: (err?: Error, errMsg?: string) => R,
   ): Promise<R>;
-};
-
-/**
- * Gets a valid representation of a value for logging or debugging purposes.
- *
- * @param value - The value to process.
- * @returns A valid representation of the value.
- */
-const getValidValue = (value: unknown): unknown => {
-  if (value instanceof Date) return value.toISOString();
-  if (typeof value === "symbol") return "<<Symbol>>";
-  if (typeof value === "function") return "<<Function>>";
-  if (typeof value === "object" && value !== null) {
-    if (Array.isArray(value)) return sortArray(value);
-    return sortObject(value);
-  }
-
-  return value;
-};
-
-/**
- * Sorts an array by getting valid representations of its elements.
- *
- * @param arr - The array to sort.
- * @returns The sorted array.
- */
-export const sortArray = (arr: unknown[]): unknown[] => {
-  if (!Array.isArray(arr)) return arr;
-  return arr.map(getValidValue).sort();
-};
-
-/**
- * Sorts an object by getting valid representations of its values.
- *
- * @param obj - The object to sort.
- * @returns The sorted object.
- */
-export const sortObject = (obj: object): { [key: string]: unknown } => {
-  if (typeof obj !== "object" || obj === null) return obj;
-  const keys = Object.keys(obj).sort((a, b) => (a > b ? 1 : -1));
-
-  const sortedEntries = Object.fromEntries(
-    keys.map((key) => {
-      const valueKey = getValidValue(obj[key as keyof typeof obj]);
-
-      return [key, valueKey];
-    }),
-  );
-  return sortedEntries;
-};
-
-/**
- * Stringifies a value.
- *
- * @param value - The value to stringify.
- * @returns The stringified representation of the value.
- */
-export const stringifyData = (value: unknown): string => {
-  if (typeof value === "string") return value;
-  try {
-    if (value instanceof Date) return getValidValue(value) as string;
-    if (value && typeof value === "object") {
-      if (Array.isArray(value)) return JSON.stringify(sortArray(value));
-
-      return JSON.stringify(sortObject(value));
-    }
-    if (!value) return String(value);
-
-    return JSON.stringify(value);
-  } catch (error) {
-    import("@utils").then(({ logger }) => {
-      logger.error("Error stringifying data:", error, value);
-    });
-    return "notValid";
-  }
-};
-
-const functionFallback = (functionName: string) => () =>
-  import("@utils").then(({ logger }) => {
-    logger.log(
-      `Function created after parsed data, original function name: "${functionName}"`,
-    );
-  });
-
-const symbolFallback = (symbolName: string) =>
-  Symbol(
-    `Symbol created after parsed data, original symbol name: "${symbolName}"`,
-  );
-
-const getCorrectParsed = <T = object | null>(obj: object | null): T => {
-  if (!obj) return null as T;
-  if (Array.isArray(obj))
-    return obj.map((value) => {
-      if (value === "<<Function>>") return functionFallback(value);
-      if (value === "<<Symbol>>") return symbolFallback(value);
-      if (typeof value === "object") return getCorrectParsed<T>(value);
-      return value;
-    }) as T;
-  else
-    return Object.fromEntries(
-      Object.entries(obj).map(([key, value]) => {
-        if (value === "<<Function>>") return [key, functionFallback(key)];
-        if (value === "<<Symbol>>") return [key, symbolFallback(key)];
-        if (typeof value === "object") return [key, getCorrectParsed(value)];
-        return [key, value];
-      }),
-    ) as T;
-};
-
-export const parseData = <T = object | null>(
-  value: string | null,
-): T | null => {
-  let parsed: T;
-  try {
-    if (!value) return value as T;
-
-    if (value.includes("<<Symbol>>") || value.includes("<<Function>>")) {
-      const parsedValue = JSON.parse(value);
-
-      return getCorrectParsed<T>(parsedValue);
-    } else parsed = JSON.parse(value || "null") as T;
-  } catch {
-    parsed = value as T;
-  }
-  return parsed;
 };
 
 const saveDataStorage: SaveDataStorage = wrapFunctionWithError(
