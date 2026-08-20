4a24e17c00dc65e6afd268e2f50a624e192ed9ad
DEPREMU <zae4758@gmail.com>
2026-08-20 22:44:41 +0000

    debugging 6

--- app/plugins/handleCreateFiles.js
+++ app/plugins/handleCreateFiles.js
@@ -2,7 +2,6 @@
 import fs from "fs";
 import path from "path";
 import { Chalk } from "chalk";
-import { execSync } from "child_process";
 const chalk = new Chalk({ level: 1 });
 
 const withGoogleServices = (config) => {
@@ -11,98 +10,20 @@ const withGoogleServices = (config) => {
 
   console.log(chalk.blue("Plugin handleCreateFiles.js is being executed..."));
 
-  if (process.env.GOOGLE_SERVICES_JSON) {
-    try {
-      console.log(
-        chalk.blue(
-          "Creating google-services.json from environment variable...",
-        ),
-      );
+  const projectRoot = config._internal?.projectRoot || process.cwd();
+  const files = fs.readdirSync(projectRoot, { recursive: true });
 
-      const projectRoot = config._internal?.projectRoot || process.cwd();
-      const files = fs.readdirSync(projectRoot, { recursive: true });
-
-      const buildGradle = files.find((file) =>
-        file.endsWith("app/build.gradle"),
-      );
-      console.log(files.filter((f) => f.includes("google-services.json")));
-
-      console.log(
-        chalk.red("build.gradle path:"),
-        path.join(projectRoot, path.dirname(buildGradle)),
-      );
-
-      const googleServicesPath = path.join(
-        projectRoot,
-        path.dirname(buildGradle),
-        "google-services.json",
-      );
-      if (fs.existsSync(googleServicesPath)) {
-        console.log(
-          chalk.yellow("google-services.json already exists at:"),
-          googleServicesPath,
-        );
-        return config;
-      }
-
-      const googleServicesContent = Buffer.from(
-        process.env.GOOGLE_SERVICES_JSON,
-        "base64",
-      ).toString("utf8");
-
-      const parsed = JSON.parse(googleServicesContent);
-      console.log(
-        chalk.blue("Valid JSON with project_id:"),
-        parsed.project_info?.project_id,
-      );
-
-      //TODO: Delete after feature finished
-      execSync("sudo apt-get install tree", { stdio: "inherit" });
-      execSync("tree -L 8", { stdio: "inherit", cwd: projectRoot });
-
-      try {
-        fs.mkdirSync(path.dirname(googleServicesPath), { recursive: true });
-      } catch (e) {
-        console.error(
-          chalk.red("Error creating directory:"),
-          e instanceof Error ? e.message : e,
-        );
-      }
-      fs.writeFileSync(googleServicesPath, googleServicesContent);
-      console.log(
-        chalk.green("google-services.json created successfully in:"),
-        googleServicesPath,
-      );
-
-      if (!fs.existsSync(googleServicesPath))
-        throw new Error("File was not created successfully");
-
-      console.log(chalk.green("File verified to exist"));
-      const stats = fs.statSync(googleServicesPath);
-      console.log("File size:", stats.size, "bytes");
-    } catch (error) {
-      console.error(chalk.red("Error creating google-services.json:"), error);
-      throw error;
-    }
-  } else {
-    console.error(
-      chalk.red(
-        "GOOGLE_SERVICES_JSON environment variable not found in EAS Build",
-      ),
-    );
-    console.error(
-      chalk.yellow("Available env vars:"),
-      Object.keys(process.env).join("\n"),
-    );
+  if (files.some((f) => f.endsWith("app/google-services.json"))) return config;
+  else
     throw new Error(
       chalk.red(
-        "GOOGLE_SERVICES_JSON environment variable is required for EAS builds",
+        `google-services.json not found, make sure the file is in the app directory ${
+          projectRoot?.endsWith("app")
+            ? path.join(projectRoot, "google-services.json")
+            : path.join(projectRoot, "app", "google-services.json")
+        }`,
       ),
     );
-  }
-
-  console.log(chalk.green("Plugin handleCreateFiles.js finished."));
-  return config;
 };
 
 export default withGoogleServices;
